506c8af114724a158815cca38e9e6c3ad6ae4866
Gouthamraj R <83597052+Gouthamraj2004@users.noreply.github.com>
2024-10-15 09:45:59 +0530

    Add files via upload

---
+++ Basic Java Programs/Even_odd.java
@@ -0,0 +1,10 @@
+class Even_odd{
+public static void main(String[]args){
+System.out.println("Checking if the number is Even or Odd");
+int n=43;
+if(n%2==0)
+System.out.println("The Number is even");
+else
+System.out.println("The Number is odd");
+}
+}
