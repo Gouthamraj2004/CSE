c822ab71f71fb514789e53879ccc8ebb82380540
Gouthamraj R <83597052+Gouthamraj2004@users.noreply.github.com>
2024-10-16 09:32:19 +0530

    new code to check positive or negative

---
+++ Basic Java Programs/Positive_Negative.java
@@ -0,0 +1,13 @@
+class Positive_Negative{
+  public static void main (String[]args){
+    int num = 5;
+    if (num >= 0){
+        if (num == 0)
+            System.out.println ("Zero");
+        else
+            System.out.println ("The number is positive");
+    }
+    else
+        System.out.println ("The number is negative");
+  }
+}
