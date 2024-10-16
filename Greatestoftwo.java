97704e3d353ab9ebd955facd765f15bc4fadb1fe
Gouthamraj R <83597052+Gouthamraj2004@users.noreply.github.com>
2024-10-16 09:41:13 +0530

    TO check Greatest of 2 Number

---
+++ Greatestoftwo.java
@@ -0,0 +1,14 @@
+class Greatestoftwo{
+    public static void main(String[]args){
+        int a=7,b=10;
+        if(a==b){
+            System.out.println("Both are Equal");
+        }
+        else if(a>b){
+            System.out.println("A is the Greatest Number");
+        }
+        else{
+            System.out.println("B is the Greatest Number");
+        }
+    }
+}
